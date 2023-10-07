6df2cb1f2d9dc6517bda17be7ff00cb17193dae4
Alex Park <a.park@tipsterchat.com>
2023-10-07 20:58:59 +0200

    cities: add sorters and filterers on cities entity

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { CitiesList } from "./components/cities/CitiesList/CitiesList";
+import { FiltersAndSortersContainer } from "./components/cities/FiltersAndSortersContainer/FiltersAndSortersContainer";
 import { CitiesProvider } from "./components/ctx/CitiesProvider";
 import { useGeoLocationContext } from "./components/ctx/GeoLocationProvider";
 import { LoadingDisplayer } from "./components/shared/loading/LoadingDisplayer";
@@ -12,6 +13,7 @@ function App() {
         <LoadingDisplayer />
       ) : (
         <CitiesProvider>
+          <FiltersAndSortersContainer />
           <CitiesList />
         </CitiesProvider>
       )}
